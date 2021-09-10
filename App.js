1d6ac00d087a99608ff12fe8584d40a87725ad46
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-10 20:03:07 -0300

    Encerramos a apostila 07, introducao ao desenvolvimento com React Native

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//useState é um hook do React.
+//Eles existem desde a versão 16.8
+import React, { useState } from 'react';
+import {Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor (props){
+    super(props)
+    this.state = {
+      texto: 'Texto Inicial',
+      contador: 0
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <Button 
+          title="Alterar o texto"
+          // onPress={() => this.setState({texto: "Novo texto"})}
+          onPress={() => this.setState({contador: this.state.contador + 1})}
+        />
+      </View>
+    )
+  }
 }
+
+// export default function App() {
+//   const [texto, setTexto] = useState('Texto Inicial')
+//   return (
+//     <View style={styles.container}>
+//       <Text>{texto}</Text>
+//       <Button 
+//         title="Alterar texto"
+//         onPress={() => setTexto('Novo texto')}
+//       />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
